9229a46b4db67245c34269e7ee326f8f9089302c
Matt <matthew.m.miller@protonmail.com>
2022-05-06 15:25:03 -0400

    Initial push

---
+++ src/main/java/me/milsky/configconsumer/RateController.java
@@ -0,0 +1,39 @@
+package me.milsky.configconsumer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RefreshScope //allows config to be refreshed without restart
+@Controller
+public class RateController {
+
+    @Value("${rate}")
+    String rate;
+
+    @Value("${lanecount}")
+    String lanecount;
+
+    @Value("${tollstart}")
+    String tollstart;
+
+    @Value("${connstring}")
+    String connstring;
+
+
+    @RequestMapping("/rate")
+    public String getRate(Model m) {
+
+        m.addAttribute("rateamount", rate);
+        m.addAttribute("lanes", lanecount);
+        m.addAttribute("tollstart", tollstart);
+        m.addAttribute("connstring", connstring);
+
+        return "rateview";
+
+    }
+
+
+}
